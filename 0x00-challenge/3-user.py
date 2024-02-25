c9313b6a5fb9c40b1691a61b275044309324ac1c
Nita Youssef <nitayoussef3@gmail.com>
2024-02-25 01:29:37 -0800

    nn

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -40,7 +40,8 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            # SOLUTION: CHANGE ._ TO .__
 
     def is_valid_password(self, pwd):
         """
@@ -54,8 +55,8 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
-
+        return hashlib.md5(pwd.encode()).hexdigest() == self.password
+        # SOLTUION: REMOVE .UPPER() ON HASH; CAPITALIZATION MISMATCHED PWD
 
 if __name__ == '__main__':
     print("Test User")
@@ -86,11 +87,11 @@ if __name__ == '__main__':
 
     if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
-password")
+ password")
 
     if user_1.is_valid_password("Fakepwd"):
         print("is_valid_password should return False if it's not the right \
-password")
+ password")
 
     if user_1.is_valid_password(None):
         print("is_valid_password should return False if compare with None")
@@ -100,4 +101,4 @@ password")
 
     if user_2.is_valid_password("No pwd"):
         print("is_valid_password should return False if no password set \
-before")
+ before")
